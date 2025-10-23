e0d9eaff7027585cde048f5d68f4f6a866d1f9d9
AboumalikAhmed <aboumalikahmed25@gmail.com>
2025-10-23 17:03:16 +0100

    modification in 23/10

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 const count_title = document.querySelector(".count_title");
 
-
-
-
 // let arrayOfChar = ;
 
 let hero_title = document.createElement("h4");
@@ -10,51 +7,23 @@ hero_title.id = "hero_title";
 let text = document.createTextNode("event details");
 let arrayOfChar = text.textContent.split("");
 
-
-
-
 counter = 0;
-let text_timer = setInterval(()=>{
-    
+let text_timer = setInterval(() => {
+  if (counter == arrayOfChar.length - 1) {
+    clearInterval(text_timer);
+  }
 
-    if(counter == arrayOfChar.length - 1 )
-    {
-        clearInterval(text_timer);
-    }
+  hero_title.textContent += arrayOfChar[counter];
+  document.querySelector(".hero_title").append(hero_title);
 
-
-
-    hero_title.textContent += arrayOfChar[counter];
-    document.querySelector(".hero_title").append(hero_title);
-
-
-
-
-     
-    counter++;
-   
-    
-
-
-    
-    
-    
-
-    
-    
-
-    
-    
-},200)
-
+  counter++;
+}, 200);
 
 let span = document.createElement("span");
 span.className = "first_word";
-span.textContent="Event";
+span.textContent = "Event ";
 span.style.color = "#FFD400";
- let title = count_title.querySelector("h3").prepend(span);
-
-
+let title = count_title.querySelector("h3").prepend(span);
 
 let days = 12;
 let hours = 1;
@@ -65,59 +34,39 @@ let hours_value = document.getElementById("hours_value");
 let minutes_value = document.getElementById("minutes_value");
 let seconds_value = document.getElementById("seconds_value");
 
-let count_down = setInterval(()=>{
+let count_down = setInterval(() => {
+  days_value.textContent = days < 10 ? "0" + days : days;
+  hours_value.textContent = hours < 10 ? "0" + hours : hours;
+  minutes_value.textContent = minutes < 10 ? "0" + minutes : minutes;
+  seconds_value.textContent = second < 10 ? "0" + second : second;
 
-
-
-
-
-
-   days_value.textContent = days < 10 ? "0"+days : days;
-   hours_value.textContent = hours < 10 ? "0"+hours : hours;
-   minutes_value.textContent  = minutes < 10 ? "0"+minutes : minutes;
-   seconds_value.textContent = second < 10 ? "0"+second : second;
-
-
-   second--;
-   if(second ==0)
-   {
-    second = 60 ;
+  second--;
+  if (second == 0) {
+    second = 60;
     minutes--;
-   }
+  }
 
-
-   if(minutes == 0){
+  if (minutes == 0) {
     minutes = 60;
     hours--;
-   }
+  }
 
-
-   if(hours == 0)
-   {
+  if (hours == 0) {
     hours = 24;
     days--;
-   }
-
-
-
-
-},1000)
-
-
-
-
-
+  }
+}, 1000);
 
 let image_event = document.querySelector(".image_event");
 let overlay = document.getElementById("overlay_jazz_gallery");
 let close_btn = document.querySelector(".close");
 
+image_event.addEventListener("click", () => {
+  overlay.classList.add("show");
+});
 
-image_event.addEventListener("click", ()=>{
-   overlay.classList.add("show");
-})
+close_btn.addEventListener("click", () => {
+  overlay.classList.remove("show");
+});
 
 
-close_btn.addEventListener("click" , ()=>{
-    overlay.classList.remove("show");
-})
